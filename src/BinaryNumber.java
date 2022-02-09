7389b66cf1086f2bce1276120c314cb87e759ff8
David Chibueze Ogbonna <davidchibzogbonna@Davids-Macbook.local>
2022-02-08 22:56:39 -0500

    initial commit

---
+++ src/BinaryNumber.java
@@ -0,0 +1,32 @@
+
+public class BinaryNumber {
+    private int data[];
+    private boolean overflow;
+
+
+    public  BinaryNumber (int length){
+        String binaryNum = " ";
+        for (int i = 0; i < length; i++) {
+            binaryNum += "0";
+        }
+    }
+    public  BinaryNumber (String str){
+        for (int i = 0; i < length; i++){
+            
+            }
+        }
+    }
+    public int getLength(){
+        
+    }
+    public int getDigit (int index) {
+        for (i = 0; )
+    }
+    public int 
+
+
+
+
+public static void main(String[] args) {
+    
+}
